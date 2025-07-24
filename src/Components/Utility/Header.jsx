283f988531a03f8abef5304c037a7316b1881e89
Clifford <cliffordatuchukwu92@gmail.com>
2025-07-24 23:38:58 +0200

    Added a Home and Favourites path for users to easily switch between pages

--- src/Components/Utility/Header.jsx
+++ src/Components/Utility/Header.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Header({ search, updateParam }) {
   return (
     <header className="header">
       <h3 class="header-title">ReactCast</h3>
+      <Link to="/favourite">
+        <button>⭐ Go to Favorites</button>
+      </Link>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
 
       <div className="search">
         <input
