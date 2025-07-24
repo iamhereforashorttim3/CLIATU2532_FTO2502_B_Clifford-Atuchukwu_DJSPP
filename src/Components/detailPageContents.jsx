e3c34960c02e68722bdb91f8a4f04058d18fc907
Clifford <cliffordatuchukwu92@gmail.com>
2025-07-24 09:22:50 +0200

    Created a button that when clicked returns the user back to the landing page

--- src/Components/detailPageContents.jsx
+++ src/Components/detailPageContents.jsx
@@ -1,14 +1,26 @@
 import SeasonDetail from "./SeasonDetail";
 import GenresApi from "./utility/genreApi";
+import { useNavigate } from "react-router-dom";
 
 export default function DetailPageContents({ show, selectedSeason }) {
   console.log("DetailPageContents show", show);
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
   const totalEpisodes = show.seasons?.reduce((sum, season) => {
     return sum + (season.episodes?.length || 0);
   }, 0);
 
   return (
     <>
+      <div>
+        <button onClick={goHome} className="return-home-button">
+          Return to Home
+        </button>
+      </div>
       <div className="detail-header">
         <div className="left-content">
           <h1 className="show-title">{show.title}</h1>
