d5465c0d1b447ca098bcc431904314b64e6d96da
Clifford <cliffordatuchukwu92@gmail.com>
2025-07-25 05:56:57 +0200

    Fixed the dropdown by adding Null checks and maintaining state

--- src/Components/Utility/seasonSelector.jsx
+++ src/Components/Utility/seasonSelector.jsx
@@ -1,30 +1,19 @@
-/**
- *
- * @param {object} component props
- * @param {Array<object>} props.seasons - An array of season objects
- * @param {object} props.selectedSeason - The selected season object
- * @returns {JSX.Element} A select dropdown with the season options
- */
 export default function SeasonSelector({ seasons, selectedSeason, onChange }) {
-  function handleChange(e) {
-    const seasonNumber = parseInt(e.target.value);
-    const selected = seasons.find((s) => s.season === seasonNumber);
-    onChange(selected);
-  }
-
   return (
-    <div className="season-selector">
-      <select
-        id="season-select"
-        onChange={handleChange}
-        value={selectedSeason?.season || ""}
-      >
-        {seasons.map((s) => (
-          <option key={s.season} value={s.season}>
-            season {s.season}
-          </option>
-        ))}
-      </select>
-    </div>
+    <select
+      value={selectedSeason?.season || seasons[0]?.season}
+      onChange={(e) => {
+        const season = seasons.find(
+          (s) => s.season === parseInt(e.target.value)
+        );
+        if (season) onChange(season);
+      }}
+    >
+      {seasons?.map((s) => (
+        <option key={s.season} value={s.season}>
+          Season {s.season}
+        </option>
+      ))}
+    </select>
   );
 }
